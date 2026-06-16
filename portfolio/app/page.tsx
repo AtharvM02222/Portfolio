74ca48766f6eea5883de68672dc666e4e0097d47
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-06-16 17:12:43 +0530

    add

---
+++ portfolio/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <center>
+        <h1>Under Dev shifting from html css js to NEXT</h1>
+      </center>
+    </main>
+  );
+}
