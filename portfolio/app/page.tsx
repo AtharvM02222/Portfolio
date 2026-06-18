0f10732a12b5d9400be209fdf161b1e59ff21586
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-06-18 12:31:49 +0530

    fix

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,10 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <center>
-        <h1>Under Dev shifting from html css js to NEXT</h1>
-      </center>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="text-4xl font-bold text-center">
+          Under Dev: Shifting from HTML/CSS/JS to Next.js
+        </h1>
+        <Image
+          src="/img/rocket.png"
+          alt="Rocket"
+          width={200}
+          height={200}
+          priority
+          className="animate-bounce"
+        />
+      </div>
     </main>
   );
 }
