da6a8505ead9243272e60e92753c8f21f1b0e2e4
Atharv Mandlavdiya <atharvam682@gmail.com>
2026-09-02 09:36:54 +0530

    feat: interactive-signature

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { signature } from "atharv-mandlavdiya";
+import { sign } from "atharv-mandlavdiya";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Portfolio", signature: signature() });
+  res.render("index", { title: "Portfolio", signature: sign() });
 });
 
 export default router;
